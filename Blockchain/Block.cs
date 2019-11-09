835c5dbd9eca7f1869868a460c91f8a1be36b51a
Raymond Selvik <raymond.d.selvik@gmail.com>
2019-11-09 18:24:09 +0100

    init project + added Block Class

---
+++ Blockchain/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+
+namespace Blockchain
+{
+    [Serializable]
+    class Block
+    {
+        //private DateTime timestamp;
+        private string previousHash;
+        private List<string> transactions;
+
+        private string blockHash;
+
+        public Block(string previousHash, List<string> transactions)
+        {
+            //this.timestamp = DateTime.UtcNow;
+            this.previousHash = previousHash;
+            this.transactions = transactions;
+            this.blockHash = CalculateBlockHash(); 
+        }
+
+        string CalculateBlockHash()
+        {
+            MD5 md5 = MD5.Create();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, this);
+
+                var hash = md5.ComputeHash(memoryStream.ToArray());
+                blockHash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+
+                memoryStream.Close();
+                return blockHash;
+            }
+        }
+
+        public string GetBlockHash()
+        {
+            return blockHash;
+        }
+    }
+}
---
+++ Blockchain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> transactions = new List<string>();
+            transactions.Add("Hfhgfello");
+            transactions.Add("dddd");
+
+            Block genesisBlock = new Block(null, transactions);
+
+            List<String> transactions2 = new List<string>();
+            transactions2.Add("øøok,e");
+
+            Block block2 = new Block(genesisBlock.GetBlockHash(), transactions2);
+
+            Console.WriteLine(genesisBlock.GetBlockHash());
+            Console.WriteLine(block2.GetBlockHash());
+        }
+    }
+}
